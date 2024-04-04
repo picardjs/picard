3fb2599c87c0a0ae556d1ca68879630f9be63287
Florian Rappl <mail@florian-rappl.de>
2024-04-04 13:31:52 +0200

    Added dts generation

--- tools/build-one.mjs
+++ tools/build-one.mjs
@@ -1,9 +1,14 @@
 import { build } from 'esbuild';
+import { generateDeclaration } from 'dets';
 import { resolve } from 'node:path';
+import { writeFile } from 'node:fs/promises';
 
 export async function buildOne(app) {
-  const entryPoints = [resolve(process.cwd(), `src/apps/${app}.ts`)];
+  const entryPoints = {
+    picard: resolve(process.cwd(), `src/apps/${app}.ts`),
+  };
   const outdir = resolve(process.cwd(), `dist/${app}`);
+  const declName = resolve(outdir, 'picard.d.ts');
   const platform = app === 'client' ? 'browser' : app === 'server' ? 'node' : 'neutral';
 
   console.log(`Building "${app}" ...`);
@@ -24,6 +29,15 @@ export async function buildOne(app) {
     platform,
     format: 'cjs',
   });
-  
+
+  const declText = await generateDeclaration({
+    name: 'picard-js',
+    files: [],
+    noModuleDeclaration: true,
+    types: Object.values(entryPoints),
+  });
+
+  await writeFile(declName, declText, 'utf8');
+
   console.log(`Finished "${app}".`);
 }
